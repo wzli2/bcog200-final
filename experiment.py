6cccd6821b6c491a032e4ce6c43cff8ca2e87ade
wzli2 <wzli2@illinois.edu>
2025-04-07 23:47:05 -0500

    experiment.py

--- experiment.py
+++ experiment.py
@@ -2,9 +2,12 @@ class Exp:
 	def __init__(self, gui):
 
 	def create_instructions(self):
+		#pulls up instruction text based on selected task
 
 	def create_sequence(self):
+		#generates a sequence based on selected task
 
 	def run_experiment(self):
+		#runs choose_task, show_instructions, and show_sequence from Gui class
 
 	def save_data(self):
