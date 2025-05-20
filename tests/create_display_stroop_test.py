3e2b7e9b418c7f1ae09510eba1b3556ea6297513
wzli2 <wzli2@illinois.edu>
2025-05-20 01:29:20 -0500

    Add files via upload

---
+++ tests/create_display_stroop_test.py
@@ -0,0 +1,52 @@
+import random
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry(f"{1200}x{775}")
+root.title("Stroop test")
+root.configure(bg="white")
+root.resizable(False, False)
+
+stroop_labels = []
+
+def create_and_display_stroop_sequence(iteration):
+	colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black"]
+	color_list = random.choices(colors, k=iteration)
+	sequence = " ".join(color_list)
+
+# make sure sequence (correct answers) is correct
+	print(f"Correct sequence string: {sequence}")
+
+	row = 0
+	column = 0
+	row_limit = 6
+	for color in color_list:
+		word = random.choice(colors)
+		stroop_label = tk.Label(root, anchor='center', text=word, bg="white", fg=color, font=("helvetica", 40))
+		stroop_label.grid(row=row, column=column, padx=40, pady=40)
+		stroop_labels.append(stroop_label)
+
+		column +=1
+		if column >= row_limit:
+			column = 0
+			row += 1
+	
+	root.update()
+
+	duration = iteration * 2500
+
+	root.after(duration, lambda: after_display(duration))
+
+
+def after_display(duration):
+	print(f"Display test successful. Elapsed time: {duration} milliseconds.")
+	root.destroy()
+
+
+def main():
+	iteration = 3
+	create_and_display_stroop_sequence(iteration)
+
+if __name__ == '__main__':
+    main()
+    root.mainloop()
